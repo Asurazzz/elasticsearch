030e9538d93ca91183b67506b1be23c1046b319f
ymj <2513667783@qq.com>
2021-09-08 15:35:54 +0800

    添加springboot_restclient

---
+++ es01_springboot_restclient/src/main/java/com/ymj/Es01SpringbootRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.ymj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es01SpringbootRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Es01SpringbootRestclientApplication.class, args);
+    }
+
+}
