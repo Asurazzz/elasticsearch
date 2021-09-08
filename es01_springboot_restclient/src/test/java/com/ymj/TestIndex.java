ee17b9d67469567ada477dc510b0d1ab643e56ce
ymj <2513667783@qq.com>
2021-09-08 17:22:46 +0800

    添加文档

--- es01_springboot_restclient/src/test/java/com/ymj/TestIndex.java
+++ es01_springboot_restclient/src/test/java/com/ymj/TestIndex.java
@@ -1,6 +1,9 @@
 package com.ymj;
 
+import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
@@ -18,6 +21,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Classname TestIndex
@@ -128,6 +135,39 @@ public class TestIndex {
         // 得到响应
         boolean acknowledged = delete.isAcknowledged();
         System.out.println("======================" + acknowledged + "======================");
+    }
+
+
+    /**
+     * 添加文档
+     * POST /elasticsearch_test/_doc/1
+     * {
+     *   "name": "spring cloud实战",
+     *   "description": "本课程主要从四个章节进行讲解： 1.微服务架构入门 2.spring cloud 基 础入门 3.实战Spring Boot 4.注册中心eureka。",
+     *   "studymodel": "201001",
+     *   "timestamp": "2020-08-22 20:09:18",
+     *   "price": 5.6
+     * }
+     * @throws IOException
+     */
+    @Test
+    public void testAddDoc() throws IOException {
+        //创建索引请求对象
+        IndexRequest indexRequest = new IndexRequest("elasticsearch_test", "doc");
+        indexRequest.id("1");
+        //文档内容 准备json数据
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("name", "spring cloud实战");
+        jsonMap.put("description", "本课程主要从四个章节进行讲解： 1.微服务架构入门 2.spring cloud 基础入门 3.实战Spring Boot 4.注册中心eureka。");
+        jsonMap.put("studymodel", "201001");
+        SimpleDateFormat dateFormat =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        jsonMap.put("timestamp", dateFormat.format(new Date()));
+        jsonMap.put("price", 5.6f);
+        indexRequest.source(jsonMap);
+        //通过client进行http的请求
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        DocWriteResponse.Result result = indexResponse.getResult();
+        System.out.println("======================" + result + "======================");
     }
 }
 
