79c1c9eca3b0212f5aa3de7d37fda0adad5e7a12
ymj <2513667783@qq.com>
2021-09-08 17:42:13 +0800

    搜索全部记录

--- es01_springboot_restclient/src/test/java/com/ymj/TestIndex.java
+++ es01_springboot_restclient/src/test/java/com/ymj/TestIndex.java
@@ -1,11 +1,14 @@
 package com.ymj;
 
+import org.apache.lucene.search.TotalHits;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
@@ -16,6 +19,10 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +30,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -185,6 +193,64 @@ public class TestIndex {
         // 得到文档的内容
         Map<String, Object> sourceAsMap = getResponse.getSourceAsMap();
         System.out.println(sourceAsMap);
+    }
+
+
+    /**
+     * 搜索全部记录
+     * GET /elasticsearch_test/_search
+     * {
+     *   "query": {
+     *     "match_all": {}
+     *   }
+     * }
+     * @throws IOException
+     * @throws ParseException
+     */
+    @Test
+    public void testSearchAll() throws IOException, ParseException {
+        // 搜索请求对象
+        SearchRequest searchRequest = new SearchRequest("elasticsearch_test");
+        // 搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        // 搜索方式
+        // matchAllQuery搜索全部
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        // 设置源字段过滤,第一个参数结果集包括哪些字段，第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[] {"name","studymodel","price","timestamp"}, new String[]{});
+        // 向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        // 执行搜索,向ES发起http请求
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 搜索结果
+        SearchHits hits = searchResponse.getHits();
+        // 匹配到的总记录数
+        TotalHits totalHits = hits.getTotalHits();
+        // 得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        // 日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            // 文档的主键
+            String id = hit.getId();
+            // 源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            // 由于前边设置了源文档字段过滤，这时description是取不到的
+            String description = (String) sourceAsMap.get("description");
+            // 学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            // 价格
+            Double price = (Double) sourceAsMap.get("price");
+            // 日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+            System.out.println("======================" + name + "======================");
+            System.out.println("======================" + studymodel + "======================");
+            System.out.println("======================" + description + "======================");
+        }
+
+
     }
 }
 
